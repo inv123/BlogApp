cd11cc3830003d4659a69ced2ae0ded5c04f4834
inv123 <nedyalko.tiholov@yahoo.com>
2021-11-21 21:51:32 +0200

    use state

---
+++ my-app/src/components/homepage/Notifications.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { BiTrendingUp } from 'react-icons/bi';
+
+
+export const Notifications = ({ submenu }) => {
+
+
+    return (
+        <>
+            <ul className={`dropdown-menu drop-notification ${submenu === 0 ? 'hide' : 'show'}`} aria-labelledby="navbarDropdownMenuLink">
+                <li>
+                    <a className="dropdown-item" href="#">
+                        <div className='not-box'>
+                            <div className='not-icon'><BiTrendingUp /></div>
+                            <div className='not-content'>
+                                <h6>analitic</h6>
+                                <p>Your website’s active users count increased by<span className='green'> 28%</span> in the last week. Great job!</p>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li style={{ padding: '10px 0' }}>
+                    <a className='dropdown-item' href='#' style={{ padding: '10px 0', background: '#ccc' }}>
+                        <h6 style={{ textAlign: 'center' }}>View all notifications</h6>
+                    </a>
+                </li>
+
+            </ul>
+        </>
+    )
+}
--- my-app/src/components/homepage/TopNav.js
+++ my-app/src/components/homepage/TopNav.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import avatar from '../../images/avatar.jpg';
 import { AiFillCaretDown } from 'react-icons/ai';
 import { BsBellFill } from 'react-icons/bs';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { BiTrendingUp } from 'react-icons/bi';
 
 import { BiUser } from 'react-icons/bi';
 import { IoMdSettings } from 'react-icons/io';
@@ -11,13 +10,18 @@ import { BsFolder2Open } from 'react-icons/bs';
 import { GrTransaction } from 'react-icons/gr';
 import { AiOutlinePoweroff } from 'react-icons/ai';
 
+import { Notifications } from './Notifications';
+
 
 export const TopNav = () => {
+    const [notificationState, setNotificationState] = useState(0);
+    const [userClick, setUserClick] = useState(0);
+
 
     //Put different padding on Count notification
     return (
         <>
-            <div className='top-nav'>
+            <div className='right-block'>
                 <nav className="navbar navbar-expand-lg navbar-light">
                     <div className="container-fluid">
 
@@ -29,40 +33,24 @@ export const TopNav = () => {
                                         <input type='text' placeholder='Search here...'></input>
                                     </form>
                                 </li>
-                                <li className="nav-item  nav-notification">
+                                <li className="nav-item  nav-notification" onClick={() => notificationState === 1 ? setNotificationState(0) : setNotificationState(1)}>
                                     <div className='notification'>
                                         <BsBellFill />
                                         <div className='notification-count'>
-                                            <p>22+</p>
+                                            <p>22</p>
                                         </div>
                                     </div>
-                                    <ul className="dropdown-menu drop-notification hide" aria-labelledby="navbarDropdownMenuLink">
-                                        <li>
-                                            <a className="dropdown-item" href="#">
-                                                <div className='not-box'>
-                                                    <div className='not-icon'><BiTrendingUp /></div>
-                                                    <div className='not-content'>
-                                                        <h6>analitic</h6>
-                                                        <p>Your website’s active users count increased by<span className='green'> 28%</span> in the last week. Great job!</p>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li style={{ padding: '10px 0' }}>
-                                            <a className='dropdown-item' href='#' style={{ padding: '10px 0', background: '#ccc' }}>
-                                                <h6 style={{ textAlign: 'center' }}>View all notifications</h6>
-                                            </a>
-                                        </li>
 
-                                    </ul>
+                                    <Notifications submenu={notificationState}/>
+
                                 </li>
-                                <li className="nav-item dropdown nav-user">
+                                <li className="nav-item dropdown nav-user" onClick={() => userClick === 0 ? setUserClick(1) : setUserClick(0)}>
                                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         <img src={avatar}></img>
                                         <p>Username</p>
                                         <AiFillCaretDown />
                                     </a>
-                                    <ul className="dropdown-menu hide" aria-labelledby="navbarDropdownMenuLink">
+                                    <ul className={`dropdown-menu ${userClick === 0 ? 'hide' : 'show'}`} aria-labelledby="navbarDropdownMenuLink">
                                         <li><a className="dropdown-item" href="#"><BiUser size='16px' style={{ marginRight: '10px' }} /> Profile</a></li>
                                         <li><a className="dropdown-item" href="#"><IoMdSettings size='16px' style={{ marginRight: '10px' }} />Settings</a></li>
                                         <li><a className="dropdown-item" href="#"><BsFolder2Open size='16px' style={{ marginRight: '10px' }} />Files</a></li>
